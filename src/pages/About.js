0a05ef9c6cdeae4eef40b157cb1b114b2d81f523
Luke Duggan <48234476+devluked@users.noreply.github.com>
2022-01-06 19:33:24 -0800

    updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,45 @@
 function About() {
-	return (
-		<section id='about' className='about'>
-			<h1>I graduated from Cal State Fullerton in 2021.</h1>
-			<h2>With a bachelors degree in Computer Science.</h2>
-			<div className='jobs'>
-				<div className='job'>
-					<h3>Feb 2020 - Jan 2021</h3>
-					<h4>Edwards Life Sciences</h4>
-					<h5>Legal Ops Associate</h5>
-					<h4>
-						Supported and managed technical side of legal department consisting
-						of 20+ attorneys. Opened tickets in ServiceNow to resolve IT related
-						issues out of my scope and to document changes made to SaaS
-						solutions.
-					</h4>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <>
+      <section id="about" className="about">
+        <h1>I graduated from Cal State Fullerton in 2021.</h1>
+        <h2>With a bachelors degree in Computer Science.</h2>
+        <div className="jobs">
+          <div className="job">
+            <h3>July 2021 - Present</h3>
+            <h4>Bridge Event Technology</h4>
+            <h5>React Engineer</h5>
+            <h4>
+              • Started position as an intern working on small tickets in Trello
+              board until moving into a Jr. position
+              <br />• Worked on bug fixes and added new features to the front
+              end such as bulk operations, UI improvement and flow
+              <br />• Created new end points on the server for fetching data
+              from the API and making POST requests to the database
+              <br />• Refactored class components into functional components
+              while also shifting components to styled components
+              <br />• Created pull requests on GitLab for senior developers to
+              review before merging into master
+            </h4>
+          </div>
+        </div>
+
+        <div className="jobs">
+          <div className="job">
+            <h3>Feb 2020 - Jan 2021</h3>
+            <h4>Edwards Life Sciences</h4>
+            <h5>Legal Ops Associate</h5>
+            <h4>
+              • Supported and managed technical side of legal department
+              consisting of 20+ attorneys. <br />• Opened tickets in ServiceNow
+              to resolve IT related issues out of my scope and to document
+              changes made to SaaS solutions.
+            </h4>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default About;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,58 +1,65 @@
-import echologo from '../assets/echo-logo.png';
+import echologo from "../assets/echo-logo.png";
+import lambo from "../assets/lambo.png";
 
 function Projects() {
-	return (
-		<section id='projects' className='projects'>
-			<div className='projects-container'>
-				<div className='projects-project'>
-					<h1>Echo Radio</h1>
-					<hr />
-					<img src={echologo} />
-					<div className='icons-container'>
-						<div className='icons'>
-							<a target='_blank' href='https://echo-radio.herokuapp.com/'>
-								<i className='eye icon'></i>Preview
-							</a>
-							<a target='_blank' href='https://github.com/devluked/echo-radio'>
-								<i className='alternate github icon'></i>Github
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className='projects-project'>
-					<h1>Echo Radio</h1>
-					<hr />
-					<img src={echologo} />
-					<div className='icons-container'>
-						<div className='icons'>
-							<a target='_blank' href='https://echo-radio.herokuapp.com/'>
-								<i className='eye icon'></i>Preview
-							</a>
-							<a target='_blank' href='https://github.com/devluked/echo-radio'>
-								<i className='alternate github icon'></i>Github
-							</a>
-						</div>
-					</div>
-				</div>
+  return (
+    <section id="projects" className="projects">
+      <div className="projects-container">
+        <div className="projects-project">
+          <h1>Echo Radio</h1>
+          <hr />
+          <img src={echologo} />
+          <div className="icons-container">
+            <div className="icons">
+              <a target="_blank" href="https://echo-radio.herokuapp.com/">
+                <i className="eye icon"></i>Preview
+              </a>
+              <a target="_blank" href="https://github.com/devluked/echo-radio">
+                <i className="alternate github icon"></i>Github
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="projects-project">
+          <h1>SoCal Vinverify</h1>
+          <hr />
+          <img
+            style={{ borderRadius: "10px", filter: "grayscale(100%)" }}
+            src={lambo}
+          />
+          <div className="icons-container">
+            <div className="icons">
+              <a target="_blank" href="https://www.socalvinverify.com/">
+                <i className="eye icon"></i>Preview
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/devluked/sheila-website"
+              >
+                <i className="alternate github icon"></i>Github
+              </a>
+            </div>
+          </div>
+        </div>
 
-				<div className='projects-project'>
-					<h1>Echo Radio</h1>
-					<hr />
-					<img src={echologo} />
-					<div className='icons-container'>
-						<div className='icons'>
-							<a target='_blank' href='https://echo-radio.herokuapp.com/'>
-								<i className='eye icon'></i>Preview
-							</a>
-							<a target='_blank' href='https://github.com/devluked/echo-radio'>
-								<i className='alternate github icon'></i>Github
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+        <div className="projects-project">
+          <h1>Echo Radio</h1>
+          <hr />
+          <img src={echologo} />
+          <div className="icons-container">
+            <div className="icons">
+              <a target="_blank" href="https://echo-radio.herokuapp.com/">
+                <i className="eye icon"></i>Preview
+              </a>
+              <a target="_blank" href="https://github.com/devluked/echo-radio">
+                <i className="alternate github icon"></i>Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
